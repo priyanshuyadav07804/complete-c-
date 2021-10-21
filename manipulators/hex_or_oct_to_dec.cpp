ec14c8b6fe47492f7e912064e24e4e1fa246536d
priyanshu yadav <80442935+priyanshuyadav07804@users.noreply.github.com>
2021-10-21 03:00:58 -0600

    Add files via upload

---
+++ manipulators/hex_or_oct_to_dec.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int h,o;
+    cout<<"enter hexal value : ";
+    cin>>hex>>h;
+    cout<<"enter octal value : ";
+    cin>>oct>>o;
+    cout<<"hexal value "<<h<<" in decimal -> "<<dec<<h<<'\n';
+    cout<<"octal value "<<o<<" in decimal -> "<<dec<<o<<'\n';
+    
+    return 0;
+}
