e4e09f25ffdda1f7e912fec11babb2a352c16ce6
priyanshu yadav <80442935+priyanshuyadav07804@users.noreply.github.com>
2021-10-21 03:04:49 -0600

    Add files via upload

---
+++ manipulators/ends.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long double pi=3.141592653589793;
+    // cout<<"HELLO"<<ends;
+    // cout<<"WORLD";
+    cout<<setw(8)<<22<<endl;
+    cout<<setw(8)<<4444<<endl;
+    cout<<setw(8)<<666666<<endl;
+    cout<<setfill('#');
+    cout<<setw(4)<<22<<setw(6)<<44<<'\n';
+    cout<<setw(6)<<22<<setw(8)<<44<<endl;
+    cout<<setbase(16)<<10<<setbase(8)<<10<<endl;
+    cout<<setprecision(5)<<pi<<endl;
+    cout<<setprecision(8)<<pi<<endl;
+
+ 
+
+    return 0;
+}
